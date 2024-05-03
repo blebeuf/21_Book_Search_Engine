20fbd4b9f5b3cf4eafe970c04363c85321638d6a
blebeuf <bryanlebeuf@gmail.com>
2024-05-03 18:27:45 -0500

    inital schema migration

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,42 @@
+import { gql } from '@apollo/server/express4';
+
+const typeDefs = gql`
+    type User {
+        _id: ID!
+        username: String!
+        email: String!
+        bookCount: Int
+        savedBooks: [Book]
+    }
+    type Auth {
+        token: ID!
+        user: User
+    }
+    type Query {
+        me: User
+    }
+    type Book {
+        bookId: ID!
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
+    input InputBook {
+        bookId: String
+        authors: [String]
+        title: String
+        description: String
+        image: String
+        link: String
+    }
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(newBook: InputBook!): User
+        removeBook(bookId: ID!): User
+    }
+`;
+
+module.exports = typeDefs;
